bac3a85b432d76d92c1fdbe67b7e29e935bde9c1
luis <luishsilva09@gmail.com>
2022-04-07 18:05:23 -0300

    funcaoadicionar quantidade de cartas que o usuario pede

--- scripts.js
+++ scripts.js
@@ -2,3 +2,11 @@ let nCartas = 0;
 while ( nCartas < 4 || nCartas%2 !== 0 || nCartas > 14){
    nCartas = prompt("Qual o numero de cartas? ");
 }
+for(let i =nCartas; i > 0;i--){
+   let cartas = document.querySelector(".cartas");
+   cartas.innerHTML += `
+      <div class="carta-front">
+         <img src="images/front.png ">
+      </div>`
+   
+}

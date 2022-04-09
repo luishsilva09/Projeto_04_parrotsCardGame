0a2a360eb5e0769fd31c30fb03ecca12f4e90fe6
luis <luishsilva09@gmail.com>
2022-04-09 19:12:35 -0300

    virar carta, selecionar duas e fazer a comparação, randomizar cartas

--- scripts.js
+++ scripts.js
@@ -1,12 +1,95 @@
 let nCartas = 0;
-while ( nCartas < 4 || nCartas%2 !== 0 || nCartas > 14){
+const cartasBack = [
+   "./images/explodyparrot.gif",
+   "./images/bobrossparrot.gif",
+   "./images/fiestaparrot.gif",
+   "./images/metalparrot.gif",
+   "./images/revertitparrot.gif",
+   "./images/tripletsparrot.gif",
+   "./images/unicornparrot.gif"]
+
+cartasBack.sort(comparador);
+function comparador() {
+   return Math.random() - 0.5;
+}
+
+let cartasViradas = []
+function clickCarta(element) {
+   cartasViradas.push(element)
+
+   if (cartasViradas.length < 3) {
+      element.classList.add("flip")
+
+      if (cartasViradas.length === 2) {
+         segundoClique(element);
+      }
+
+   }
+
+}
+
+function segundoClique(element) {
+   console.log(cartasViradas)
+   let carta1 = cartasViradas[0].querySelector('.back').getAttribute("src")
+   let carta2 = cartasViradas[1].querySelector('.back').getAttribute("src")
+   console.log(carta1)
+   console.log(carta2)
+   console.log(cartasViradas)
+   match(carta1, carta2)
+
+}
+
+function desvirar() {
+   cartasViradas[0].classList.remove("flip")
+   cartasViradas[1].classList.remove("flip")
+   cartasViradas = []
+   console.log(cartasViradas)
+
+}
+
+function match(carta1, carta2) {
+   if (carta1 === carta2) {
+      cartasViradas = []
+   } else {
+      setTimeout(desvirar, 1000)
+   }
+}
+
+
+function iniciarjogo() {
    nCartas = prompt("Qual o numero de cartas? ");
+   while (nCartas < 4 || nCartas % 2 !== 0 || nCartas > 14) {
+      nCartas = prompt("Qual o numero de cartas? ");
+   }
+   adicionarCartas()
 }
-for(let i =nCartas; i > 0;i--){
-   let cartas = document.querySelector(".cartas");
-   cartas.innerHTML += `
-      <div class="carta-front">
-         <img src="images/front.png ">
+
+
+function adicionarCartas() {
+   for (let i = nCartas / 2; i > 0; i--) {
+      let cartas = document.querySelector(".jogo-memoria");
+      cartas.innerHTML += `
+      <div class="carta" onclick="clickCarta(this)">   
+         <div class="carta-front">   
+            <img  src="./images/front.png " >
+         </div>
+         <div class="carta-back">
+            <img  class="back" src="${cartasBack[i]}" >
+         </div>
       </div>`
-   
+
+   }
+   for (let i = nCartas / 2; i > 0; i--) {
+      let cartas = document.querySelector(".jogo-memoria");
+      cartas.innerHTML += `
+      <div class="carta" onclick="clickCarta(this)">   
+         <div class="carta-front">   
+            <img  src="./images/front.png " >
+         </div>
+         <div class="carta-back">
+            <img  class=" back"src="${cartasBack[i]}" >
+         </div>
+      </div>`
+   }
 }
+iniciarjogo()
